61981055e898fc161617e8ed645adebdccd3f7ca
dindc <dindc@ethz.ch>
2018-11-20 17:14:33 +0100

    Merging Projects:
    - Put station board on vuforia target
    - GAme Input curser, gaze manager... --> HoloToolkit

---
+++ DisplayStationBoard/Assets/Scripts/VideoOnClick.cs
@@ -0,0 +1,23 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoOnClick : MonoBehaviour {
+
+    public GameObject go;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var vp = go.GetComponent<VideoPlayer>();
+        if (vp.isPlaying)
+        {
+            vp.Pause();
+        }
+        else
+        {
+            vp.Play();
+        }
+    }
+}
