cbaa8bad4b42cb83e519f0b6888e4c094c23f7dd
pimklaassen <pim.o.klaassen@gmail.com>
2018-12-18 13:47:36 +0100

    Create Twitter Interface

--- DisplayStationBoard/Assets/Scripts/Twitter/TwitterClickHandler.cs
+++ DisplayStationBoard/Assets/Scripts/Twitter/TwitterClickHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class TwitterClickHandler : MonoBehaviour
@@ -9,6 +10,7 @@ public class TwitterClickHandler : MonoBehaviour
     public GameObject tweetPanel;
     public GameObject tweetPrefab;
     private TextMesh textObject;
+    public GameObject[] tweetbuttonlist = new GameObject[4];
 
     public void Start()
     {
@@ -20,29 +22,39 @@ public class TwitterClickHandler : MonoBehaviour
             switch (EventSystem.current.currentSelectedGameObject.tag)
             {
                 case "trump":
-                    textObject.text = Main.tweetList[0];
-
-                    //updateButtons(Main.alltweets[0]);
+                    //textObject.text = Main.tweetList[0];
+                    updateButtons(Main.alltweets[0]);
                     break;
                 case "tagesanzeiger":
-                    textObject.text = Main.tweetList[1];
+                    //textObject.text = Main.tweetList[1];
+                    updateButtons(Main.alltweets[1]);
                     break;
                 case "zvv":
-                    textObject.text = Main.tweetList[2];
+                    //textObject.text = Main.tweetList[2];
+                    updateButtons(Main.alltweets[2]);
                     break;
                 case "reuters":
-                    textObject.text = Main.tweetList[3];
+                    //textObject.text = Main.tweetList[3];
+                    updateButtons(Main.alltweets[3]);
                     break;
-            }
+        }
     }
 
     void updateButtons (List<string> alltweets)
     {
         for (int i = 0; i < Main.alltweets.Count; i++)
         {
+            Destroy(tweetbuttonlist[i]);
             GameObject button = (GameObject)Instantiate(tweetPrefab);
+            tweetbuttonlist[i] = button;
             button.name = string.Format("{0}", i);
             button.transform.SetParent(tweetPanel.transform);
+
+            button.transform.GetChild(0).GetComponent<RectTransform>().offsetMin = new Vector2(15, 0);
+            button.transform.GetChild(0).GetComponent<Text>().text = alltweets[i];
+            button.transform.GetChild(0).GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+
+            button.GetComponentInChildren<Text>().color = new Color32(0xFF, 0xFF, 0xFF, 0xFF);
         }
     }
 }
